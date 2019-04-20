54ff88fbbc048750702afd271a0677f3ced8f0de
Abhishek Patil <AbhishekPatil@gatech.edu>
2019-04-20 05:00:36 -0400

    abhis changes

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,45 +2,42 @@
  * pulls information from the form and build the query URL
  * @returns {string} URL for NYT API based on form inputs
  */
-function buildQueryURL() {
-    // queryURL is the url we'll use to query the API
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
-  
-    // Begin building an object to contain our API call's query parameters
-    // Set the API key
-    var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
-  
-    // Grab text the user typed into the search input, add to the queryParams object
-    queryParams.q = $("#search-term")
-      .val()
-      .trim();
-  
-    // If the user provides a startYear, include it in the queryParams object
+  // API doesn't have a "limit" parameter, so we have to do this ourselves
+var numArticles = 10
+// Function to empty out the articles
+function clear() {
+  $("#article-section").empty();
+}
 
+function buildQueryURL() {
+   // Grab text the user typed into the search input, add to the queryParams object
+  var queryPara = $("#search-term")
+  .val()
+  .trim();
   
-    // Logging the URL so we have access to it for troubleshooting
-    console.log("---------------\nURL: " + queryURL + "\n---------------");
-    console.log(queryURL + $.param(queryParams));
-    return queryURL + $.param(queryParams);
+  // queryURL is the url we'll use to query the API
+    var queryURL = `https://newsapi.org/v2/everything?q=${queryPara}&domains=nytimes.com&sortBy=publishedAt&apiKey=ee6ef7c523284d20a68b8ac070bfe2a5`;
+  
+  
+    return queryURL
   }
   
-  /**
-   * takes API data (JSON/object) and turns it into elements on the page
-   * @param {object} NYTData - object containing NYT API data
-   */
-  function updatePage(NYTData) {
-    // Get from the form the number of results to display
-    // API doesn't have a "limit" parameter, so we have to do this ourselves
-    var numArticles = 5
-  
-    // Log the NYTData to console, where it will show up as an object
-    console.log(NYTData);
-    console.log("------------------------------------");
-  
-    // Loop through and build elements for the defined number of articles
-    for (var i = 0; i < numArticles; i++) {
+  $("#run-search").on("click", function() {
+    clear();
+    // Build the query URL for the ajax request to the NYT API
+    var wQueryURL = buildQueryURL();
+    console.log(wQueryURL)
+    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+    // The data then gets passed as an argument to the updatePage function
+    $.ajax({
+      url: wQueryURL,
+      method: "GET"
+    })
+    .then(function(response){
+     console.log(response)
+     for (var i = 0; i < numArticles; i++) {
       // Get specific article info for current index
-      var article = NYTData.response.docs[i];
+      var article = response.articles[i];
   
       // Increase the articleCount (track article # - starting at 1)
       var articleCount = i + 1;
@@ -53,78 +50,208 @@ function buildQueryURL() {
       $("#article-section").append($articleList);
   
       // If the article has a headline, log and append to $articleList
-      var headline = article.headline;
+      var headline = article.title;
       var $articleListItem = $("<li class='list-group-item articleHeadline'>");
   
-      if (headline && headline.main) {
-        console.log(headline.main);
+      if (headline) {
+        console.log(headline);
         $articleListItem.append(
           "<span class='label label-primary'>" +
             articleCount +
             "</span>" +
             "<strong> " +
-            headline.main +
+            headline +
             "</strong>"
         );
       }
   
       // If the article has a byline, log and append to $articleList
-      var byline = article.byline;
+      var byline = article.author;
   
-      if (byline && byline.original) {
-        console.log(byline.original);
-        $articleListItem.append("<h5>" + byline.original + "</h5>");
-      }
-  
-      // Log section, and append to document if exists
-      var section = article.section_name;
-      console.log(article.section_name);
-      if (section) {
-        $articleListItem.append("<h5>Section: " + section + "</h5>");
+      if (byline) {
+        console.log(byline);
+        $articleListItem.append("<h5>" + byline + "</h5>");
       }
   
       // Log published date, and append to document if exists
-      var pubDate = article.pub_date;
-      console.log(article.pub_date);
+      var pubDate = article.publishedAt;
+      console.log(article.publishedAt);
       if (pubDate) {
-        $articleListItem.append("<h5>" + article.pub_date + "</h5>");
+        $articleListItem.append("<h5>" + article.publishedAt+ "</h5>");
       }
   
       // Append and log url
-      $articleListItem.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
-      console.log(article.web_url);
+      $articleListItem.append("<a href='" + article.url + "'>" + article.url + "</a>");
+      console.log(article.url);
   
       // Append the article
       $articleList.append($articleListItem);
     }
-  }
-  
-  // Function to empty out the articles
-  function clear() {
-    $("#article-section").empty();
-  }
-  
-  // CLICK HANDLERS
-  // ==========================================================
-  
-  // .on("click") function associated with the Search Button
-  $("#run-search").on("click", function(event) {
-    // This line allows us to take advantage of the HTML "submit" property
-    // This way we can hit enter on the keyboard and it registers the search
-    // (in addition to clicks). Prevents the page from reloading on form submit.
-    event.preventDefault();
-  
-    // Build the query URL for the ajax request to the NYT API
-    var queryURL = buildQueryURL();
-  
-    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-    // The data then gets passed as an argument to the updatePage function
-    $.ajax({
-      url: queryURL,
-      method: "GET"
     })
-    .then(updatePage);
   });
+  
+  
 
-  //  .on("click") function associated with the clear button
-$("#clear-all").on("click", clear);
+//WSJ
+//queryURL is the url we'll use to query the API
+function buildWSJQueryURL() {
+  
+  var queryParams = $("#search-term")
+  .val()
+  .trim();
+  var wQueryURL = `https://newsapi.org/v2/everything?q=${queryParams}&domains=wsj.com&sortBy=publishedAt&apiKey=ee6ef7c523284d20a68b8ac070bfe2a5`
+  console.log (wQueryURL);
+  return wQueryURL
+}
+
+// .on("click") function 
+$("#wsj-search").on("click", function() {
+  clear();
+  // Build the query URL for the ajax request to the NYT API
+  var wQueryURL = buildWSJQueryURL();
+  console.log(wQueryURL)
+  // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+  // The data then gets passed as an argument to the updatePage function
+  $.ajax({
+    url: wQueryURL,
+    method: "GET"
+  })
+  .then(function(response){
+   console.log(response)
+   for (var i = 0; i < numArticles; i++) {
+    // Get specific article info for current index
+    var article = response.articles[i];
+
+    // Increase the articleCount (track article # - starting at 1)
+    var articleCount = i + 1;
+
+    // Create the  list group to contain the articles and add the article content for each
+    var $articleList = $("<ul>");
+    $articleList.addClass("list-group");
+
+    // Add the newly created element to the DOM
+    $("#article-section").append($articleList);
+
+    // If the article has a headline, log and append to $articleList
+    var headline = article.title;
+    var $articleListItem = $("<li class='list-group-item articleHeadline'>");
+
+    if (headline) {
+      console.log(headline);
+      $articleListItem.append(
+        "<span class='label label-primary'>" +
+          articleCount +
+          "</span>" +
+          "<strong> " +
+          headline +
+          "</strong>"
+      );
+    }
+
+    // If the article has a byline, log and append to $articleList
+    var byline = article.author;
+
+    if (byline) {
+      console.log(byline);
+      $articleListItem.append("<h5>" + byline + "</h5>");
+    }
+
+    // Log published date, and append to document if exists
+    var pubDate = article.publishedAt;
+    console.log(article.publishedAt);
+    if (pubDate) {
+      $articleListItem.append("<h5>" + article.publishedAt+ "</h5>");
+    }
+
+    // Append and log url
+    $articleListItem.append("<a href='" + article.url + "'>" + article.url + "</a>");
+    console.log(article.url);
+
+    // Append the article
+    $articleList.append($articleListItem);
+  }
+  })
+});
+
+
+//BBC
+//queryURL is the url we'll use to query the API
+function buildBQueryURL() {
+  
+  var queryParam = $("#search-term")
+  .val()
+  .trim();
+
+  var bQueryURL = `https://newsapi.org/v2/everything?q=${queryParam}&domains=bbc.co.uk&sortBy=publishedAt&apiKey=ee6ef7c523284d20a68b8ac070bfe2a5`
+  console.log (bQueryURL);
+  return bQueryURL
+}
+
+// .on("click") function 
+$("#bbc-search").on("click", function() {
+  clear();
+  // Build the query URL for the ajax request to the NYT API
+  var bQueryURL = buildBQueryURL();
+  console.log(bQueryURL)
+  // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+  // The data then gets passed as an argument to the updatePage function
+  $.ajax({
+    url: bQueryURL,
+    method: "GET"
+  })
+  .then(function(response){
+   console.log(response)
+   for (var i = 0; i < numArticles; i++) {
+    // Get specific article info for current index
+    var article = response.articles[i];
+
+    // Increase the articleCount (track article # - starting at 1)
+    var articleCount = i + 1;
+
+    // Create the  list group to contain the articles and add the article content for each
+    var $articleList = $("<ul>");
+    $articleList.addClass("list-group");
+
+    // Add the newly created element to the DOM
+    $("#article-section").append($articleList);
+
+    // If the article has a headline, log and append to $articleList
+    var headline = article.title;
+    var $articleListItem = $("<li class='list-group-item articleHeadline'>");
+
+    if (headline) {
+      console.log(headline);
+      $articleListItem.append(
+        "<span class='label label-primary'>" +
+          articleCount +
+          "</span>" +
+          "<strong> " +
+          headline +
+          "</strong>"
+      );
+    }
+
+    // If the article has a byline, log and append to $articleList
+    var byline = article.author;
+
+    if (byline) {
+      console.log(byline);
+      $articleListItem.append("<h5>" + byline + "</h5>");
+    }
+
+    // Log published date, and append to document if exists
+    var pubDate = article.publishedAt;
+    console.log(article.publishedAt);
+    if (pubDate) {
+      $articleListItem.append("<h5>" + article.publishedAt+ "</h5>");
+    }
+
+    // Append and log url
+    $articleListItem.append("<a href='" + article.url + "'>" + article.url + "</a>");
+    console.log(article.url);
+
+    // Append the article
+    $articleList.append($articleListItem);
+  }
+  })
+});
